c766725f45bcff1f6c43a0427551641dd522c82e
megahomyak <g.megahomyak@gmail.com>
2023-06-19 21:45:56 +0500

    Added support for uppercase "ey"

--- content.js
+++ content.js
@@ -12,7 +12,7 @@ function isVisible(element) {
     return isVisible;
 }
 
-function makeReplacer(vowels, repl, alphabet, replEnd) {
+function makeReplacer(vowels, repl, alphabet, replEnd, caseInsensitive) {
     let regexp = new RegExp(
         `(?!${repl})` // Excluding words that already end with "ey"
         + `(?:`
@@ -23,17 +23,19 @@ function makeReplacer(vowels, repl, alphabet, replEnd) {
         + `(?<firstLetter2>[${alphabet}])(?<consonant>(?![${vowels}])[${alphabet}])` // a letter an a consonant (which has to be saved, so it's grouped)
         + `)` // The end of the word ending's letters
         + `(?<after>[^${alphabet}]|$)`, // The end of the word (something after the word)
-        "iug" // case-Insensitive, matching Unicode, Global (don't return after first match)
+        caseInsensitive ? "iug" : "ug" // case-Insensitive, matching Unicode, Global (don't return after first match)
     );
     console.log(regexp);
     return (text) => text.replace(regexp, `$<firstLetter1>$<firstLetter2>$<consonant>${repl}$<after>`);
 }
 
-let ruReplacer = makeReplacer("аеиоуыэюяёь", "ей", "ёа-я", "й");
-let enReplacer = makeReplacer("aeiouy", "ey", "a-z", "y");
+let capitalRuReplacer = makeReplacer("АЕИОУЫЭЮЯЁЬ", "ЕЙ", "ЁА-Я", "Й", false);
+let capitalEnReplacer = makeReplacer("AEIOUY", "EY", "A-Z", "Y", false);
+let ruReplacer = makeReplacer("аеиоуыэюяёь", "ей", "ёа-я", "й", true);
+let enReplacer = makeReplacer("aeiouy", "ey", "a-z", "y", true);
 
 function modifyText(text) {
-    return ruReplacer(enReplacer(text));
+    return ruReplacer(enReplacer(capitalEnReplacer(capitalRuReplacer(text))));
 }
 
 function deteyifyElement(element) {
